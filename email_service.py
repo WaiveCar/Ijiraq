c15b2ce95fe5d185780771696cd4df5adba99300
David Alexander Leigh <daleighan@gmail.com>
2019-10-17 17:28:08 -0700

    The templates are now correctly sent as html

--- email_service.py
+++ email_service.py
@@ -14,16 +14,55 @@ config = {
     'api_key': '76d6847773f9514f75c80fd3ff4ec882-4167c382-b53ffbc6'
   }
 
-def send_message(recpient):
-  return requests.post(
-    'https://api.mailgun.net/v3/{}/messages'.format(config['domain']),
-    auth=("api", config['api_key']),
-    data={
-      'from': config['sender'],
-      'to': [config['recipient'] if 'recipient' in config else recipient],
-      'subject': 'Hello',
-      'text': 'Testing some Mailgun awesomness!'
-    }
+def send_message(recipient, subject, body):
+  try:
+    return requests.post(
+      'https://api.mailgun.net/v3/{}/messages'.format(config['domain']),
+      auth=("api", config['api_key']),
+      data={
+        'from': config['sender'],
+        'to': [config['recipient'] if 'recipient' in config else recipient],
+        'subject': subject,
+        'html': body
+      }
+    )
+  except Exception as e:
+    print('Error sending email', e);
+    return Exception
+
+def send_receipt(recipient, ad_id):
+  return send_message(
+      recipient, 
+      'Thanks For Your Oliver Purchase!', 
+      """
+        <div>
+          Ad: {}
+          Content to be added to email later
+        </div>
+      """.format(ad_id)
   )
-print(requests)
-print(send_message('daleighan@gmail.com'));
+
+def send_approval(recipient, ad_id):
+  return send_message(
+      recipient, 
+      'Your Oliver Posting Has Been Approved!', 
+      """
+        <div>
+          Ad: {}
+          Content to be added to email later
+        </div>
+      """.format(ad_id)
+  )
+
+def send_rejection(recipient, ad_id):
+  return send_message(
+      recipient, 
+      'Your Oliver Posting Has Been Rejected.', 
+      """
+        <div>
+          Ad: {}
+          Content to be added to email later
+        </div>
+      """.format(ad_id)
+  )
+
