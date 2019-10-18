b6f5aead8d6a5e33ab7ca604c1e902a261aaea01
David Alexander Leigh <daleighan@gmail.com>
2019-10-17 18:49:41 -0700

    Began adding different pages

--- Products/adcast/js/wizard.js
+++ Products/adcast/js/wizard.js
@@ -1,3 +1,40 @@
-function hello() {
-  console.log('hello');
-}
+(() => {
+  let pages = {
+    '/category': html`
+      <div>
+        Select Category
+      </div>
+    `,
+    '/targeting': html`
+      <div>
+        Select Targeting
+      </div>
+    `,
+    '/layout': html`
+      <div>
+        Select Layout
+      </div>
+    `,
+    '/info': html`
+      <div>
+        Add Info
+      </div>
+    `,
+    '/budget': html`
+      <div>
+        Edit Budget
+      </div>
+    `,
+    '/summary': html`
+      <div>
+        Summary,
+      </div>
+    `,
+    '/payment': html`
+      <div>
+        Payment
+      </div>
+    `,
+  };
+  document.querySelector('#anchor').innerHTML = 'wizard';
+})();
