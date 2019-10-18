392dd88a050d5a89e11e28e95b6080175f30881e
David Alexander Leigh <daleighan@gmail.com>
2019-10-17 19:59:13 -0700

    Adding in changing of the back and next buttons

--- Products/adcast/js/wizard.js
+++ Products/adcast/js/wizard.js
@@ -38,24 +38,35 @@
   ];
 
   let currentPage = Number(window.location.pathname.split('/').pop());
+  let backBtn = document.querySelector('#back-btn');
+  let nextBtn = document.querySelector('#next-btn');
+  let adData = {};
 
   function showPage(pageNum) {
     if (pageNum < 0 || pageNum > pages.length - 1) {
       return;
     }
+    backBtn.style.visibility = pageNum === 0 ? 'hidden' : 'visible';
+    nextBtn.textContent = pageNum !== pages.length - 1 ? 'Next' : 'Submit';
+    nextBtn.onclick =
+      pageNum !== pages.length - 1
+        ? () => showPage(currentPage + 1)
+        : () => submit(adData);
     document.querySelector('#anchor').innerHTML = pages[pageNum];
     if (currentPage !== pageNum) {
-      window.history.pushState({}, pageNum, window.location.origin + '/campaigns/wizard/' +  pageNum)
+      window.history.pushState(
+        {},
+        pageNum,
+        window.location.origin + '/campaigns/wizard/' + pageNum,
+      );
     }
     currentPage = pageNum;
   }
-  showPage(currentPage);
-  
-  document.querySelector('#forward-btn').onclick = function() {
-    showPage.call(this, currentPage + 1);
-  }
-  document.querySelector('#back-btn').onclick = function() {
-    showPage.call(this, currentPage - 1);
+
+  function submit(data) {
+    console.log('Submitting: ', data);
   }
 
+  showPage(currentPage);
+  document.querySelector('#back-btn').onclick = () => showPage(currentPage - 1);
 })();
