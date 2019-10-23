14a9089f3a1431164c818d673d2eb4d7834da029
David Alexander Leigh <daleighan@gmail.com>
2019-10-22 19:43:38 -0700

    The approach with using a redirect is now working

--- Products/adcast/js/signin.js
+++ Products/adcast/js/signin.js
@@ -1,27 +1,23 @@
-function onGoogleSignIn(googleUser) {
+function handleGoogleSignIn(googleUser) {
   // Useful data for your client-side scripts:
   let profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+  console.log('ID: ' + profile.getId()); // Don't send this directly to your server!
   console.log('Full Name: ' + profile.getName());
   console.log('Given Name: ' + profile.getGivenName());
   console.log('Family Name: ' + profile.getFamilyName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
 
   // The ID token you need to pass to your backend:
   let id_token = googleUser.getAuthResponse().id_token;
-  console.log("ID Token: " + id_token);
+  console.log('ID Token: ' + id_token);
 }
 
 (() => {
-  document.querySelector('.form-fields').innerHTML = [
-    'email',
-    'password',
-  ]
-    .map(
-      field => {
-        var type = field == 'email' ? 'text' : 'password';
-        return `
+  document.querySelector('.form-fields').innerHTML = ['email', 'password']
+    .map(field => {
+      var type = field == 'email' ? 'text' : 'password';
+      return `
     <div class="form-group">
       <input 
         name="${field}" 
@@ -33,7 +29,12 @@ function onGoogleSignIn(googleUser) {
       >
     </div>
   
-  `}
-    )
+  `;
+    })
     .join('');
+  let GoogleAuth = gapi.auth2.init();
+  if (GoogleAuth.isSignedIn.get()) {
+    let user = GoogleAuth.currentUser.get();
+    handleGoogleSignIn(user);
+  }
 })();
--- Products/adcast/js/signup.js
+++ Products/adcast/js/signup.js
@@ -1,18 +1,18 @@
 let form = document.querySelector('form');
 
-function onGoogleSignIn(googleUser) {
+function handleGoogleSignIn(googleUser) {
   // Useful data for your client-side scripts:
   let profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+  console.log('ID: ' + profile.getId()); // Don't send this directly to your server!
   console.log('Full Name: ' + profile.getName());
   console.log('Given Name: ' + profile.getGivenName());
   console.log('Family Name: ' + profile.getFamilyName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
 
   // The ID token you need to pass to your backend:
   let id_token = googleUser.getAuthResponse().id_token;
-  console.log("ID Token: " + id_token);
+  console.log('ID Token: ' + id_token);
 }
 
 function signup() {
@@ -48,4 +48,9 @@ function signup() {
   `,
     )
     .join('');
+  let GoogleAuth = gapi.auth2.init();
+  if (GoogleAuth.isSignedIn.get()) {
+    let user = GoogleAuth.currentUser.get();
+    handleGoogleSignIn(user);
+  }
 })();
