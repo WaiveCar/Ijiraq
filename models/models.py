41cd934974a7c8e2f976befdb285f89c1b2bfc2c
David Alexander Leigh <daleighan@gmail.com>
2019-10-28 15:55:54 -0700

    Added screen model

--- models/models.py
+++ models/models.py
@@ -6,6 +6,40 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////var/db/waivescreen/main.db'
 db = SQLAlchemy(app)
 
+class Screen(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  uid = db.Column(db.Text, nullable=False)
+  imei = db.Column(db.Text)
+  phone = db.Column(db.Text)
+  car = db.Column(db.Text)
+  project = db.Column(db.Text)
+  has_time = db.Column(db.Boolean, default=False)
+  app_id = db.Column(db.Integer)
+  ticker_id = db.Column(db.Integer)
+  model = db.Column(db.Text)
+  panels = db.Column(db.Text)
+  photo = db.Column(db.Text)
+  revenue = db.Column(db.Integer)
+  impact = db.Column(db.Integer)
+  lat = db.Column(db.Float, default=None)
+  lng = db.Column(db.Float, default=None)
+  location = db.Column(db.Text)
+  version = db.Column(db.Text)
+  version_time = db.Column(db.Integer)
+  uptime = db.Column(db.Integer)
+  pings = db.Column(db.Integer, default=0)
+  port = db.Column(db.Integer)
+  active = db.Column(db.Boolean, default=True)
+  removed = db.Column(db.Boolean, default=False)
+  is_fake = db.Column(db.Boolean, default=False)
+  features = db.Column(db.Text)
+  first_seen = db.Column(db.DateTime)
+  last_tast = db.Column(db.Integer, default=0)
+  last_loc = db.Column(db.DateTime)
+  last_seen = db.Column(db.DateTime)
+  ignition_state = db.Column(db.Text)
+  ignition_time = db.Column(db.DateTime)
+
 class ScreenCampaign(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   screen_id = db.Column(db.Integer)
@@ -23,6 +57,10 @@ class LocationHistory(db.Model):
   def __repr__(self):
       return '<LocationHistory %r>' % self.id
 
-SC = ScreenCampaign(screen_id=2, campaign_id=3)
-db.session.add(SC)
-db.session.commit()
+#SC = ScreenCampaign(screen_id=2, campaign_id=3)
+#db.session.add(SC)
+#db.session.commit()
+found = ScreenCampaign.query.filter_by(id=1).all()
+
+for each in found:
+  print(each.__dict__)
