d5b60a26fce2d1f341326ff3ef63ca2c3ad9aa19
Nick Thobe <nthobe@waive.com>
2019-11-06 16:18:31 -0800

    Updating Slack #screen-alerts API key

--- AdDaemon/lib/lib.php
+++ AdDaemon/lib/lib.php
@@ -811,7 +811,7 @@ function slack_alert_feature_change($old, $new) {
   $old_f = json_decode($old['features'], true);
   $new_f = json_decode($new['features'], true);
   $diff_txt = feature_diff_recurse($new_f, $old_f);
-  $slack_url = 'https://hooks.slack.com/services/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';
+  $slack_url = 'https://hooks.slack.com/services/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';
   $msg = [
     'text' => sprintf("*Feature changes on %s:*\n>%s", $old['uid'], implode("\n>", $diff_txt))
   ];
