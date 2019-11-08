7f16acfb4581e674d3afdad25ea480ce6ce5fc7e
Chris McKenzie <kristopolous@yahoo.com>
2019-11-07 19:38:11 -0800

    not considering objetion under 2 times seen

--- AdDaemon/lib/lib.php
+++ AdDaemon/lib/lib.php
@@ -1132,7 +1132,7 @@ function infer() {
       if(
         $v1[0] / $ttl < 0.45 ||
         // this tries to fix stuck gps
-        ($avg > 5000 && $ttl > 2 && $v1[0] / $ttl < 0.80)
+        ($avg > 5000 && $ttl > 3 && $v1[0] / $ttl < 0.80) || $ttl < 3
       ) {
         unset($xref[$key][$k1]);
       } else {
