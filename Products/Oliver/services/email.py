41eb96bb08546d7c757ccf441df54a0b4e5e5f03
David Alexander Leigh <daleighan@gmail.com>
2019-11-22 13:51:21 -0800

    Added in new api keys

--- Products/Oliver/services/email.py
+++ Products/Oliver/services/email.py
@@ -6,19 +6,22 @@ config = {
     'sender': 'Waive <support@waive.com>',
     'transport_name': 'mailgun',
     'domain': 'waive.com',
-    'api_key': '76d6847773f9514f75c80fd3ff4ec882-4167c382-b53ffbc6',
+    'api_key': 'key-2804ba511f20c47a3c2dedcd36e87c92',
     'recipient': 'alex@waive.com'
   } if 'ENV' not in os.environ or os.environ['ENV'] != 'production' else {
     'sender': 'Waive <support@waive.com>',
     'transport_name': 'mailgun',
     'domain': 'waive.com',
-    'api_key': '76d6847773f9514f75c80fd3ff4ec882-4167c382-b53ffbc6'
+    'api_key': 'key-2804ba511f20c47a3c2dedcd36e87c92'
   }
 
 def send_message(recipient, subject, body):
   response = requests.post(
     'https://api.mailgun.net/v3/{}/messages'.format(config['domain']),
     auth=("api", config['api_key']),
+    headers={
+        'Accept': 'application/json'
+    },
     data={
       'from': config['sender'],
       'to': [config['recipient'] if 'recipient' in config else recipient],
