6a889dd3236290d31e7a3c3fbf197f393a8878b7
David Alexander Leigh <daleighan@gmail.com>
2019-11-26 16:57:10 -0800

    Removing unused items from notices show

--- Products/Oliver/js/noticeShow.js
+++ Products/Oliver/js/noticeShow.js
@@ -12,79 +12,11 @@ function renderCampaign(campaign) {
   document.querySelector('#end-date').value = campaign.end_time.split(' ')[0];
 }
 
-function calcItems() {
-  requestAnimationFrame(() => {
-    let schedule = JSON.parse($('#schedule').jqs('export'));
-    let minutesPerWeek = schedule.reduce((acc, item) => {
-      return (
-        acc +
-        item.periods.reduce((acc, period) => {
-          return (
-            acc +
-            moment(period.end, 'hh:mm').diff(
-              moment(period.start, 'hh:mm'),
-              'minutes',
-            )
-          );
-        }, 0)
-      );
-    }, 0);
-    let budget = document.querySelector('#budget').value;
-    let fakeNumImpressionsPerWeek = budget * 14.32;
-    let fakeCPM = (fakeNumImpressionsPerWeek / budget / 100).toFixed(2);
-    if (budget) {
-      document.querySelector('#budget').textContent = `$${budget}`;
-      document.querySelector('#cpm').textContent = `$${fakeCPM}`;
-      document.querySelector(
-        '#impressions',
-      ).textContent = `${fakeNumImpressionsPerWeek}`;
-    }
-  });
-}
-
 let campaign = null;
 
-let selectedLinkIdx = 0;
-let topBarRight = document.querySelector('.top-bar-right');
-
-function changeSelected(newIdx) {
-  topBarRight.children[selectedLinkIdx].classList.remove('top-bar-selected');
-  selectedLinkIdx = newIdx;
-  topBarRight.children[selectedLinkIdx].classList.add('top-bar-selected');
-}
-
 (() => {
-  console.log('href', window.location.href);
-  document.querySelector('#campaign-url').innerHTML = `URL: ${window.location.href}`;
-  topBarRight.innerHTML = [
-    'Overview',
-    'Budget',
-    'Performance',
-    'Creatives',
-    'Billing',
-    'Summary',
-    'Location',
-  ]
-    .map(
-      (item, i) => `
-    <a href="#${item}">
-      <div class="top-bar-link" onclick="changeSelected(${i})">
-        ${item}
-      </div>
-    </a>
-  `,
-    )
-    .concat(['<div class="top-bar-link update-campaign-btn p-manager">Update</div>'])
-    .join('');
-  topBarRight.children[selectedLinkIdx].classList.add('top-bar-selected');
-
-  window.addEventListener('hashchange', function() {
-    window.scrollTo(window.scrollX, window.scrollY - 50);
-  });
-
-  $('#schedule').jqs();
   const id = new URL(location.href).searchParams.get('id');
-  fetch(`http://adcast/api/campaigns?id=${id}`)
+  fetch(`http://staging.waivescreen.com/api/campaigns?id=${id}`)
     .then(response => response.json())
     .then(json => {
       self.j = json;
@@ -100,14 +32,5 @@ function changeSelected(newIdx) {
       handleUploads(json[0].asset)
     })
     .catch(e => console.log('error fetching screens', e));
-  document
-    .getElementById('campaign-budget')
-    .addEventListener('change', calcItems);
-  document
-    .getElementById('campaign-budget')
-    .addEventListener('keyup', calcItems);
-  document
-    .querySelector('.jqs-table tbody')
-    .addEventListener('mouseup', calcItems);
 })();
 
