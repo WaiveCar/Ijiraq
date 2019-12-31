046c5a1033aee2e01a40029337d74442c4e5f1b3
Chris McKenzie <kristopolous@yahoo.com>
2019-12-30 21:38:11 -0800

    removing font weights that aren't being used

--- Products/ces/ces_oliver.php
+++ Products/ces/ces_oliver.php
@@ -6,11 +6,11 @@ $message = $Parsedown->text("
 # olvr.io 
  __The way__ to advertise for free during CES.");
 ?>
-<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Roboto+Mono:300,400,500,700,900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Roboto:400,700,900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,700,900&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700i&display=swap" rel="stylesheet"> 
 <style>
-  * { font-family: 'Roboto', sans-serif; }
+* { font-family: 'Roboto', sans-serif; }
 body {
 margin: 0;
 font-size: 120px;
@@ -35,13 +35,13 @@ background: rgba(0,0,0,0.9); color: #aef;
 padding: 0 25px;
 font-family: 'Roboto Mono', monospace; }
 #message {
-  font-weight: 400;
-  margin: 0;
-  padding: 25px 75px;
+font-weight: 400;
+margin: 0;
+padding: 25px 75px;
 }
 #message p {
-  margin: 0;
-  padding: 0;
+margin: 0;
+padding: 0;
 }
 i,em{font-family: 'Lora', serif; font-weight: bold}
 a { color: #248 }
