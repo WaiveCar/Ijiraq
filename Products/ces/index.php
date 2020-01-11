bb073dd03483581bbd73f153dabd7640a04e040c
Chris McKenzie <kristopolous@yahoo.com>
2020-01-10 21:24:14 -0800

    removing the 'free for CES'

--- Products/ces/index.php
+++ Products/ces/index.php
@@ -25,7 +25,7 @@ if(!empty($_GET['id'])) {
 ?>
 <!doctype html5>
 <head>
-  <title>Oliver, by Waive, Free for CES</title>
+  <title>Oliver, by Waive</title>
   <meta name=viewport content="width=device-width,initial-scale=1.0">
   <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,600,900&display=swap" rel="stylesheet">
   <link href="style.css" rel="stylesheet">
