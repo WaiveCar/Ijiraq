42063aec05054647ce6dab358c45bdc88eb5338b
Chris McKenzie <kristopolous@yahoo.com>
2020-03-26 14:36:10 -0700

    basic externalized self contained template engine

--- Products/adcast/template.js
+++ Products/adcast/template.js
@@ -1,34 +1,68 @@
-var id = 8;
-var exclude_list = new Set(['created_at','id']);
-function assign(node, value) {
-  let is_url = value.match(/^https?:\/\//i);
-  if(node.tagName === 'IMG') {
-    if(is_url) {
-      node.src = value;
-    }
-  } else {
-    node.innerHTML = value;
-  }
-}
+function template(opts) {
+  var
+    _res = Object.assign({
+      server: "https://staging.waivescreen.com/api/provides",
 
-window.onload = function() {
-  fetch(`http://staging.waivescreen.com/api/provides?id=${id}`)
-    .then(response => response.json())
-    .then(res => {
-      let list = Object.keys(res).filter(x => !exclude_list.has(x));
-      for (let key of list) {
-        if(Array.isArray(res[key])) {
-          for(let node of document.querySelectorAll(`.tpl-${key}`)) {
-            var which = parseInt(node.dataset.index,10);
-            if(res[key][which]) {
-              assign(node, res[key][which].url);
-            }
-          }
-        } else {
-          for(let node of document.querySelectorAll(`.tpl-${key}`)) {
-            assign(node, res[key]);
-          }
-        } 
+      db: {},
+
+      duration: 7.5,
+      id: false,
+
+      target: { width: 1920, height: 675 },
+
+      data: {},
+
+    }, opts || {});
+
+  var exclude_list = new Set(['created_at','id']);
+
+  function assign(node, value) {
+    let is_url = value.match(/^https?:\/\//i);
+    if(node.tagName === 'IMG') {
+      if(is_url) {
+        node.src = value;
       }
-    });
+    } else {
+      node.innerHTML = value;
+    }
+  }
+
+  function parser(data) {
+    let list = Object.keys(data).filter(x => !exclude_list.has(x));
+    for (let key of list) {
+      if(Array.isArray(data[key])) {
+        for(let node of document.querySelectorAll(`.tpl-${key}`)) {
+          var which = parseInt(node.dataset.index, 10);
+          if(data[key][which]) {
+            assign(node, data[key][which].url);
+          }
+        }
+      } else {
+        for(let node of document.querySelectorAll(`.tpl-${key}`)) {
+          assign(node, data[key]);
+        }
+      } 
+    }
+  }
+
+  function remote() {
+    fetch(`${_res.server}?id=${_res.id}`)
+      .then(response => response.json())
+      .then(parser)
+  }
+  
+  function load() {
+    if(_res.data){
+      parser(_res.data);
+    } else if(_res.id && _res.server) {
+      remote();
+    } else {
+      console.log("woah partner, I need either data or id + server");
+    }
+  }
+
+  load();
+  _res.load = load;
+
+  return _res;
 }
