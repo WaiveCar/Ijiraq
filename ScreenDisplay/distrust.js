753c538ecce4366c649a779bc5974c9ec6f6a8c4
Chris McKenzie <kristopolous@yahoo.com>
2020-04-12 21:23:18 -0700

    docs on the hb

--- ScreenDisplay/distrust.js
+++ ScreenDisplay/distrust.js
@@ -40,3 +40,16 @@ function amIfullScreen() {
 
 window.addEventListener("unload", () => handleVisibility(true));
 
+function hb() {
+  //
+  // we need to establish identity and our idea of 
+  //
+  //  the current time
+  //  the last time we sent something
+  //  what's currently being played
+  //
+  // not that we'll necessarily do anything with this xref but
+  // if we need to establish consistency in the future this
+  // will allow us to do so.
+  //
+}
