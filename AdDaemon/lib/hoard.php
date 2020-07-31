d44c3dc90876c9f095babccdaeb889ae10e23651
Chris McKenzie <kristopolous@yahoo.com>
2020-07-30 18:46:31 -0700

    Adding the initial hoard lib

---
+++ AdDaemon/lib/hoard.php
@@ -0,0 +1,29 @@
+<?
+
+require  'vendor/autoload.php';
+function hoard_discover($payload) {
+  // We're going to be stupid right now and do something that
+  // could potentially, through sloppy coding, lead to collisions
+  // like we had with waivescreen uids.
+  //
+  // This is MOSTLY because I've been lagging for 6 months
+  // on writing this so I have to toss ambitious dreams
+  // out the window in order to become unstuck.
+  if(!isset($payload['uid'])) {
+    // We'll trust the uid from the screen for now
+    if(isset($_SESSION['uid'])) {
+      $payload['uid'] = $_SESSION['uid'];
+    } // otherwise we have to generate a new screen id
+
+  }
+}
+
+
+use Ramsey\Uuid\Uuid;
+
+function compact_uuid() {
+  $b16 = str_replace('-', '', Uuid::uuid4()->toString());
+  return str_replace(['+','/','='], ['-','_',''], base64_encode(hex2bin($b16)));
+}
+
+echo compact_uuid();
